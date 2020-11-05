aadd2bd77bad0d48e86a37bcade661a2b07fadf0
Jonas Mayer <jonas.mayer19@gmx.de>
2020-11-05 18:49:19 +0100

    #61 Test Framework für C# einrichten
    TestRunner für Unity eingerichtet.
    Zur Nutzung TestRunner in Unity hinzufügen:
    Unity -> Window->General
    ->TestRunner

---
+++ DigitalCommissioningTool/Assets/Tests/EditMode/EditModeTestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class EditModeTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+    }
+}
