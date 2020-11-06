4ffa1555555a2c562d0313836315174579d27412
Jonas Mayer <jonas.mayer19@gmx.de>
2020-11-06 23:27:01 +0100

    #62 Test schreiben der Log Dateien prüft

---
+++ DigitalCommissioningTool/Assets/Tests/EditMode/LogManagerTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using SystemTools.ManagingRessources;
+using SystemTools.Logging;
+using System.IO;
+using System.Linq;
+
+namespace Tests
+{
+    public class LogManagerTests
+    {
+        [Test]
+        public void writes_log_message()
+        {
+            string testString = "ABCDEFG123456789";
+
+            bool infoLogMessageFound = false;
+            bool errorLogMessageFound = false;
+
+            LogManager.WriteLog(testString, LogLevel.Info, false, "LogManagerTests", "writes_log_message");
+            LogManager.WriteError(testString, "LogManagerTests", "writes_log_message");
+
+            using (ConfigManager cman = new ConfigManager())
+            {
+                cman.OpenConfigFile("Paths");
+                ConfigData logPath = cman.LoadData("LogPathDebug");
+                DirectoryInfo logDirectory = new DirectoryInfo(logPath.GetValueAsString());
+                FileInfo logFile = logDirectory.GetFiles().OrderByDescending(f => f.LastWriteTime).First();
+
+                using (StreamReader sr = logFile.OpenText())
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        if (s.Equals("[INFO] [LogManagerTests][writes_log_message] " + testString))
+                        {
+                            infoLogMessageFound = true;
+                        }
+                        else if (s.Equals("[ERROR] [LogManagerTests][writes_log_message] " + testString))
+                        {
+                            errorLogMessageFound = true;
+                        }
+                    }
+                }
+            }
+
+            Assert.IsTrue(infoLogMessageFound && errorLogMessageFound);
+        }
+    }
+}
