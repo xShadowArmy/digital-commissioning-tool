7413ee6dad8c1197c80bcf008f0f114940d13b6c
Jonas Mayer <jonas.mayer19@gmx.de>
2020-11-08 16:29:36 +0100

    #65 Testen ob Überschreiben von Daten funktioniert

---
+++ DigitalCommissioningTool/Assets/Tests/EditMode/StringResourceManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using SystemTools.ManagingRessources;
+
+namespace Tests
+{
+    public class StringResourceManagerTests
+    {
+        [Test]
+        public void overrides_string_resources()
+        {
+            string key = "testResource13425346564765";
+            StringRessourceManager.StoreString(key, "unchanged content");
+            StringRessourceManager.StoreString(key, "overwritten content", true);
+            
+            string stringResource = StringRessourceManager.LoadString("@" + key);
+
+            Assert.AreEqual("overwritten content", stringResource);
+            
+
+
+        }
+    }
+}
