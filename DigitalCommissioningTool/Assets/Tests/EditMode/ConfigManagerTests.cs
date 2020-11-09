9e36b0318ddad970fe185be55963e34437615a8d
Jonas Mayer <jonas.mayer19@gmx.de>
2020-11-09 17:27:23 +0100

    #63 Testen ob Config Dateien richtig erstellt werden

---
+++ DigitalCommissioningTool/Assets/Tests/EditMode/ConfigManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using SystemTools.ManagingResources;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ConfigManagerTests
+    {
+        public ConfigManagerTests()
+        {
+            TestConfigFilePath = ".\\Output\\Resources\\Data\\TestConfigFile2345234523.xml";
+        }
+
+        private string TestConfigFilePath { get; set; }
+
+        [Test]
+        public void creates_config_files()
+        {
+            using (ConfigManager cman = new ConfigManager())
+            {
+                cman.OpenConfigFile("TestConfigFile2345234523", true);
+            }
+            Assert.IsTrue(File.Exists(TestConfigFilePath));
+            File.Delete(TestConfigFilePath);
+        }
+
+    }
+}
