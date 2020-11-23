9c36e2fbc1a2da322478539467973d1672043c1f
Jonas Mayer <jonas.mayer19@gmx.de>
2020-11-23 13:39:55 +0100

    Schreibfehler in Test korrigiert

--- DigitalCommissioningTool/Assets/UnitTests/SystemFacade/EditModeTests/GameManagerTests.cs
+++ DigitalCommissioningTool/Assets/UnitTests/SystemFacade/EditModeTests/GameManagerTests.cs
@@ -10,13 +10,13 @@ namespace UnitTests.SystemFacade
         [Test]
         public void creates_project()
         {
-            string projectName = "TestProject3490580298539";
+            string projectName = "TestProject3490580298540";
             GameManager.CreateProject(projectName);
             GameManager.GameWarehouse.CreateWall(new Vector3(1f, 2f, 3f), new Vector3(4f, 5f, 6f), new Vector3(7f, 8f, 9f));
             GameManager.GameWarehouse.CreateDoor(new Vector3(1f, 2f, 3f), new Vector3(4f, 5f, 6f), new Vector3(77f, 88f, 99f), DoorType.Gate);
-            GameManager.GameWarehouse.CreateStorageReck(new Vector3(11f, 222f, 333f), new Vector3(4f, 5f, 6f), new Vector3(7f, 8f, 9f));
-            StorageData data0 = GameManager.GameWarehouse.CreateStorageReck(new Vector3(1f, 2f, 3f), new Vector3(433f, 555f, 666f), new Vector3(7f, 8f, 9f));
-            GameManager.GameWarehouse.CreateStorageReckItem(new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f), data0);
+            GameManager.GameWarehouse.CreateStorageRack(new Vector3(11f, 222f, 333f), new Vector3(4f, 5f, 6f), new Vector3(7f, 8f, 9f));
+            StorageData data0 = GameManager.GameWarehouse.CreateStorageRack(new Vector3(1f, 2f, 3f), new Vector3(433f, 555f, 666f), new Vector3(7f, 8f, 9f));
+            GameManager.GameWarehouse.CreateStorageRackItem(new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f), data0);
 
             StorageData data = GameManager.GameContainer.CreateContainer(new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f));
             ItemData item = GameManager.GameContainer.CreateContainerItem(new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f), data);
