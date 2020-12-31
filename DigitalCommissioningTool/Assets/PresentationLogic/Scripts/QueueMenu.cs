3dc94e45be16673e2a637e915aa1c69ea615fe68
Percy233 <64072893+Percy233@users.noreply.github.com>
2020-12-31 15:36:46 +0100

    #134, #136, MOSIM PathPlanning
    Listenansicht für Queue erstellt.
    Hinzufügen und entfernen von Einträgen ermöglicht.
    Pathplanning mit MOSIM PathPlanningService implementiert.

---
+++ DigitalCommissioningTool/Assets/PresentationLogic/Scripts/QueueMenu.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using ApplicationFacade;
+using UnityEngine.UI;
+
+public class QueueMenu : MonoBehaviour
+{
+    [SerializeField] SelectionManager selectionManager;
+    public GameObject ItemTemplate;
+    public GameObject PanelQueue;
+    public GameObject CanvasWarehouseItems;
+    public Transform WalkTargets;
+    public TestPascal Avatar;
+    [HideInInspector]
+    public DragItem ActiveItem;
+    public GameObject DraggedItem;
+    public ItemData SelectedItem;
+
+    private List<DragItem> QueueItems = new List<DragItem>();
+
+    // Start is called before the first frame update
+
+    private void updateSize()
+    {
+        float newHeight = System.Math.Max(161, 1 + (QueueItems.Count + 1) * 16);
+        RectTransform contentBox = ItemTemplate.transform.parent.GetComponent<RectTransform>();
+        contentBox.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newHeight);
+    }
+
+    void Start()
+    {
+        selectionManager.ShelveSelected += SelectionManager_ShelveSelected;
+    }
+    public void AddItemToQueue(ItemData itemData)
+    {
+        ItemTemplate.transform.Find("Name/NameText").GetComponent<TextMeshProUGUI>().text = itemData.Name;
+        ItemTemplate.transform.Find("Count/CountText").GetComponent<TextMeshProUGUI>().text = "x" + itemData.Count;
+        GameObject field = Instantiate(ItemTemplate, ItemTemplate.transform.parent);
+        field.SetActive(true);
+        DragItem dragItem = field.GetComponent<DragItem>();
+        dragItem.LinkedItem = itemData;
+        QueueItems.Add(dragItem);
+        updateSize();
+    }
+
+    public void QueueCloseClick()
+    {
+        PanelQueue.SetActive(false);
+        PanelQueue.transform.parent.gameObject.SetActive(true);
+    }
+    public void ChangeQueueOrder()
+    {
+        Avatar.WalkTrajectoryPoints.Clear();
+        foreach (DragItem item in QueueItems)
+        { 
+            Transform walkTarget = item.LinkedItem.Object.transform.Find("BoxWalkTarget");
+            Avatar.WalkTrajectoryPoints.Add(walkTarget);
+        }
+        Avatar.UpdatePath();
+    }
+    public void OnClick()
+    {
+        if (ActiveItem != null)
+        {
+            Destroy(ActiveItem.gameObject);
+            QueueItems.Remove(ActiveItem);
+        }
+        else
+        {
+            if (SelectedItem != null && !QueueItems.Exists(x => x.LinkedItem == SelectedItem))
+            {
+                AddItemToQueue(SelectedItem);
+            }
+        }
+    }
+    private void SelectionManager_ShelveSelected(GameObject selectedObject, bool active)
+    {
+        if (ActiveItem != null)
+        {
+            ActiveItem.transform.GetComponentInChildren<Image>().color = Color.white;
+            ActiveItem = null;
+        }
+        if (active)
+        {
+            SelectedItem = GameManager.GameWarehouse.GetStorageRackItem(selectedObject);
+        }
+        else
+        {
+            SelectedItem = null;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
--- DigitalCommissioningTool/Assets/PresentationLogic/Scripts/WalkTrajectory.cs
+++ DigitalCommissioningTool/Assets/PresentationLogic/Scripts/WalkTrajectory.cs
@@ -1,13 +1,19 @@
-﻿using MMIStandard;
+﻿using MMICSharp.Access;
+using MMICSharp.Adapter;
+using MMICSharp.Common.Communication;
+using MMIStandard;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityLocomotionMMU;
 
 public class WalkTrajectory : MonoBehaviour
 {
     public List<Transform> Points = new List<Transform>();
-
+    public MotionTrajectory2D ComputedPath;
     public Color Color = Color.red;
+
 
 
     /// <summary>
@@ -36,8 +42,66 @@ public class WalkTrajectory : MonoBehaviour
 
         return constraint;
     }
+    public MotionTrajectory2D GetPathConstraintCollision(MMUAccess MMUAccess, float Velocity)
+    {
+        List<MTransform> computedPath = new List<MTransform>();
+        MPathPlanningService.Iface pathPlanningService = MMUAccess.ServiceAccess.PathPlanningService;
 
 
+        List<MSceneObject> sceneObjects = MMUAccess.SceneAccess.GetSceneObjects();
+        //Remove everything except StorageRacks
+        //for (int i = sceneObjects.Count - 1; i >= 0; i--)
+        //{
+        //    if (!sceneObjects[i].Name.Contains("StorageRack"))
+        //    {
+        //        sceneObjects.RemoveAt(i);
+        //    }
+        //}
+        for (int i = 0; i < Points.Count - 1; i++)
+        {
+            MVector start = new MVector() { Values = new List<double>() { Points[i].position.x, Points[i].position.z } };
+            MVector goal = new MVector() { Values = new List<double>() { Points[i + 1].position.x, Points[i + 1].position.z } };
+            MPathConstraint result = pathPlanningService.ComputePath(start, goal, sceneObjects, new Dictionary<string, string>()
+                        {
+                            { "mode", "2D"},
+                            { "time", Serialization.ToJsonString(1.0f)},
+                            { "radius", Serialization.ToJsonString(0.3f)},
+                            { "height", Serialization.ToJsonString(0.5f)},
+                        });
+            if (result.PolygonPoints.Count > 0)
+            {
+                if (result.PolygonPoints[0].ParentToConstraint != null)
+                {
+                    computedPath.AddRange(result.PolygonPoints.Select(s => new MTransform() { Position = new MVector3(s.ParentToConstraint.Position.X, 0, s.ParentToConstraint.Position.Z) }).ToList());
+                }
+                else
+                {
+                    computedPath.AddRange(result.PolygonPoints.Select(s => new MTransform() { Position = new MVector3(s.TranslationConstraint.X(), 0, s.TranslationConstraint.Z()) }).ToList());
+                }
+            }
+            else
+            {
+                Debug.Log("No path found");
+            }
+        }
+        ComputedPath = new MotionTrajectory2D(computedPath, Velocity);
+        drawLine();
+        return ComputedPath;
+    }
+    private void drawLine() 
+    {
+        LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.positionCount = (ComputedPath.Poses.Count - 1) * 2;
+        lineRenderer.SetPosition(0, new Vector3(ComputedPath.Poses[0].Position.x, 0, ComputedPath.Poses[0].Position.y));
+        for (int i = 0; i < ComputedPath.Poses.Count - 1; i++)
+        {
+            Vector2 from = ComputedPath.Poses[i].Position;
+            Vector2 to = ComputedPath.Poses[i + 1].Position;
+            lineRenderer.SetPosition((i * 2), new Vector3(from.x, 0, from.y));
+            lineRenderer.SetPosition((i * 2) + 1, new Vector3(to.x, 0, to.y));
+            Debug.DrawLine(new Vector3(from.x, 0, from.y), new Vector3(to.x, 0, to.y), this.Color);
+        }
+    }
     private void OnDrawGizmos()
     {
         if (this.Points.Count >= 1)
