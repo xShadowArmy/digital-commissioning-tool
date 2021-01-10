e0639a19ea764c494436724d418c350f5512c2a1
ch-wengler <72490062+ch-wengler@users.noreply.github.com>
2021-01-10 12:18:12 +0100

    Speicher Button

---
+++ DigitalCommissioningTool/Assets/PresentationLogic/Scripts/SaveProject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ApplicationFacade.Application;
+
+public class SaveProject : MonoBehaviour
+{
+    public Button SaveProjectButton;
+    void Start()
+    {
+        SaveProjectButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        GameManager.SaveProject(GameManager.OpenProjectData.ProjectName);
+    }
+}
