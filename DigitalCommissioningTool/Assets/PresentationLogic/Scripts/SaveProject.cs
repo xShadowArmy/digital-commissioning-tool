d53a3fce749312c2810842a71dfbba7b11ea1a0f
ch-wengler <72490062+ch-wengler@users.noreply.github.com>
2021-01-12 21:08:29 +0100

    ProjektSpeicherButton repariert + Konsolenausgabe

--- DigitalCommissioningTool/Assets/PresentationLogic/Scripts/SaveProject.cs
+++ DigitalCommissioningTool/Assets/PresentationLogic/Scripts/SaveProject.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using SystemFacade;
 using UnityEngine;
 using UnityEngine.UI;
 using ApplicationFacade.Application;
+using TMPro;
 
 public class SaveProject : MonoBehaviour
 {
     public Button SaveProjectButton;
+    [SerializeField] private TMP_Text saveProjectText;
     void Start()
     {
         SaveProjectButton.onClick.AddListener(TaskOnClick);
+        saveProjectText.text = StringResourceManager.LoadString("@Save");
     }
 
     void TaskOnClick()
     {
         GameManager.SaveProject(GameManager.OpenProjectData.ProjectName);
+        print("Project '" + GameManager.OpenProjectData.ProjectName + "' was saved");
     }
 }
